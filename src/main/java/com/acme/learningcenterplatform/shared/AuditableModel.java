a1b41a2248ee6f1328b574437fbf2d4e8301cf8b
teochiii <u202211212@upc.edu.pe>
2024-10-07 08:39:04 -0500

    awuuauw

--- src/main/java/com/acme/learningcenterplatform/shared/AuditableModel.java
+++ src/main/java/com/acme/learningcenterplatform/shared/AuditableModel.java
@@ -1,4 +1,24 @@
 package com.acme.learningcenterplatform.shared;
 
+import java.util.Date;
+
 public class AuditableModel {
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
