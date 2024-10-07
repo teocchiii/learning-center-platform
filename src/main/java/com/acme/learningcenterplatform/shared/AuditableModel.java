3ac7cc6c3fc5b65de431e4a980f1cb1c8b55b31b
teochiii <u202211212@upc.edu.pe>
2024-10-07 09:47:41 -0500

    chore: Followed the teacher process

--- src/main/java/com/acme/learningcenterplatform/shared/AuditableModel.java
+++ src/main/java/com/acme/learningcenterplatform/shared/AuditableModel.java
@@ -1,24 +1,30 @@
 package com.acme.learningcenterplatform.shared;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.util.Date;
 
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
 public class AuditableModel {
+
+
+    @Getter
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
     private Date createdAt;
+
+
+
+    @Getter
+    @LastModifiedDate
+    @Column(nullable = false)
     private Date updatedAt;
 
-    public Date getCreatedAt() {
-        return createdAt;
-    }
 
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
